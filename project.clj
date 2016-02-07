5250840fb5a17e8e65e81971f312b054aeaf31b9
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2016-02-07 16:11:21 +0200

    Add basic info to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject reagent-spreadsheet "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Minimal Reagent component that mimics spreadsheet applications like Excel"
+  :url "https://github.com/MattiNieminen/reagent-spreadsheet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
