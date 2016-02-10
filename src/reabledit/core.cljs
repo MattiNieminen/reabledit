a46664167360cc0e75743b043f5993c6136e4300
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2016-02-10 11:05:47 +0200

    Implement navigation using arrow keys

--- src/reabledit/core.cljs
+++ src/reabledit/core.cljs
@@ -1,10 +1,40 @@
 (ns reabledit.core
   (:require [reagent.core :as reagent]))
 
+;;
+;; State-related helpers
+;;
+
+(defn set-selected!
+  [ui-state row col]
+  (swap! ui-state assoc :selected [row col]))
+
+
+;;
+;; Keyboard event handlers
+;;
+
+(defn handle-key-down
+  [e rows cols ui-state]
+  (let [keycode (.-keyCode e)
+        current-row (first (:selected @ui-state))
+        current-col (second (:selected @ui-state))
+        f (partial set-selected! ui-state)]
+    (condp = keycode
+      37 (f current-row (max 0 (dec current-col)))
+      38 (f (max 0 (dec current-row)) current-col)
+      39 (f current-row (min (dec cols) (inc current-col)))
+      40 (f (min (dec rows) (inc current-row)) current-col)
+      nil)))
+
+;;
+;; Components
+;;
+
 (defn data-table-cell
   [v nth-row nth-col ui-state]
   [:td {:class (if (= (:selected @ui-state) [nth-row nth-col]) "selected")
-        :on-click #(swap! ui-state assoc :selected [nth-row nth-col])} v])
+        :on-click #(set-selected! ui-state nth-row nth-col)} v])
 
 (defn data-table-row
   [headers row-data nth-row ui-state]
@@ -26,6 +56,8 @@
   (let [ui-state (reagent/atom {})]
     (fn [headers data]
       [:div.reabledit
+       {:tabIndex 0
+        :on-key-down #(handle-key-down % (count data) (count headers) ui-state)}
        [:table
         [data-table-headers headers]
         [:tbody
