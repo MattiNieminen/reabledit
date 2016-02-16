9f8b3fddb2e139f4ba720f355c09c3777045f9a9
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2016-02-16 13:00:55 +0200

    Change test data domain

--- example-src/devdemos/core.cljs
+++ example-src/devdemos/core.cljs
@@ -12,23 +12,18 @@
     :value "Name"}
    {:key :age
     :value "Age"
-    :editor components/int-editor}
-   {:key :owner
-    :value "Owner"}])
+    :editor components/int-editor}])
 
 (def example-data
   [{:id 1
-    :name "Rin Tin Tin"
-    :age 3
-    :owner "Lee Duncan"}
+    :name "Deadpool"
+    :age 32}
    {:id 2
-    :name "Lassie"
-    :age 10
-    :owner "Jeff"}
+    :name "Supergirl"
+    :age 27}
    {:id 3
-    :name "Krypto"
-    :age "24"
-    :owner "Clark Kent"}])
+    :name "Dr. Strange"
+    :age "43"}])
 
 (defonce example-atom (reagent/atom example-data))
 
