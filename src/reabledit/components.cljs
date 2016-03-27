8ad7157663bba1a80f1836e3848708140f43b506
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2016-03-27 18:09:41 +0300

    Replace the use of cursors with values and functions

--- src/reabledit/components.cljs
+++ src/reabledit/components.cljs
@@ -26,65 +26,64 @@
 
 (defn string-editor
   []
-  (fn [cursor _]
+  (fn [v change-fn _]
     [:input {:type "text"
              :auto-focus true
              :on-focus util/move-cursor-to-end!
-             :value @cursor
-             :on-change #(reset! cursor (-> % .-target .-value))}]))
+             :value v
+             :on-change #(change-fn (-> % .-target .-value))}]))
 
 (defn int-editor
   []
-  (fn [cursor _]
+  (fn [v change-fn _]
     [:input {:type "text"
              :auto-focus true
              :on-focus util/move-cursor-to-end!
-             :value @cursor
+             :value v
              :on-change (fn [e]
                           (let [new-value (js/parseInt (-> e .-target .-value))
                                 int? (not (js/isNaN new-value))]
                             (if int?
-                              (reset! cursor new-value))))}]))
+                              (change-fn new-value))))}]))
 
 (defn- dropdown-editor-on-key-down
-  [e cursor options]
+  [e v change-fn options]
   (let [keycode (.-keyCode e)
-        position (first (keep-indexed #(if (= %2 @cursor) %1)
+        position (first (keep-indexed #(if (= %2 v) %1)
                                       (map :key options)))]
     (case keycode
       38 (do
            (.preventDefault e)
            (if (zero? position)
-             (reset! cursor (-> options last :key))
-             (reset! cursor (:key (nth options (dec position))))))
+             (change-fn (-> options last :key))
+             (change-fn (:key (nth options (dec position))))))
       40 (do
            (.preventDefault e)
            (if (= position (-> options count dec))
-             (reset! cursor (-> options first :key))
-             (reset! cursor (:key (nth options (inc position))))))
+             (change-fn (-> options first :key))
+             (change-fn (:key (nth options (inc position))))))
       nil)))
 
 (defn dropdown-editor
   [options]
   (with-meta
-    (fn [cursor disable-edit!]
-      (let [chosen-key @cursor]
-        [:div.reabledit-dropdown
-         {:tabIndex 0
-          :on-key-down #(dropdown-editor-on-key-down % cursor options)}
-         [:span (-> (filter #(= (:key %) chosen-key) options)
-                    first
-                    :value)]
-         [:div.reabledit-dropdown-items
-          (for [{:keys [key value]} options]
-            ^{:key key}
-            [:div.reabledit-dropdown-item
-             {:class (if (= key chosen-key) "selected")
-              :on-click (fn [e]
-                          (.stopPropagation e)
-                          (reset! cursor key)
-                          (disable-edit!))}
-             [:span value]])]]))
+    (fn [v change-fn disable-edit!]
+      [:div.reabledit-dropdown
+       {:tabIndex 0
+        :on-key-down #(dropdown-editor-on-key-down % v change-fn options)}
+       [:span (-> (filter #(= (:key %) v) options)
+                  first
+                  :value)]
+       [:div.reabledit-dropdown-items
+        (for [{:keys [key value]} options]
+          ^{:key key}
+          [:div.reabledit-dropdown-item
+           {:class (if (= key v) "selected")
+            :on-click (fn [e]
+                        (.stopPropagation e)
+                        (change-fn key)
+                        (disable-edit!))}
+           [:span value]])]])
     {:component-did-mount
      (fn [this]
        (.focus (reagent/dom-node this)))}))
@@ -101,13 +100,15 @@
     (fn [columns data v nth-row nth-col state
          enable-edit! disable-edit! set-selected!]
       (let [column (nth columns nth-col)
-            cursor (reagent/cursor state [:edit :updated (:key column)])
             view (or (:view column) (span-view))
             editor (or (:editor column) (string-editor))]
         [:div.reabledit-cell {:class (if @selected? "selected")
                               :on-click #(set-selected! nth-row nth-col)}
          (if (and @selected? @edit?)
-           [editor cursor disable-edit!]
+           (let [path [:edit :updated (:key column)]
+                 value-in-state (get-in @state path)
+                 change-fn #(swap! state assoc-in path %)]
+             [editor value-in-state change-fn disable-edit!])
            [view v enable-edit!])]))))
 
 (defn data-table-row
